86b4bb13765e74c1f3909b7d9640560b146a70ea
JosepMariaProfitos <josepprofitos99@gmail.com>
2020-02-12 16:21:40 +0100

    commit Historial

--- src/main/java/modelo/Historial.java
+++ src/main/java/modelo/Historial.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author alu2019265
- */
+import java.util.Date;
+
+
 public class Historial {
     
+    private String username;
+    private Date fechaEvento;
+
+    /**
+     * Get the value of username
+     *
+     * @return the value of username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @param username new value of username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
