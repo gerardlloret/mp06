ebb3a1332d258fe3614536d9ddca0149405aff6b
Roger <>
2020-02-12 16:19:14 +0100

    creacion modelo.inciencias, enum de tipos de incidencia.

--- src/main/java/modelo/Incidencia.java
+++ src/main/java/modelo/Incidencia.java
@@ -5,11 +5,56 @@
  */
 package modelo;
 
+import enums.Type;
 import java.util.Date;
 
 
 public class Incidencia {
 
     private Date fecha;
+    private Type tipo;
+    private Empleado origen;
+    private Empleado destino;
+
+    public Incidencia(Date fecha, Type tipo, Empleado origen, Empleado destino) {
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.origen = origen;
+        this.destino = destino;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Type getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Type tipo) {
+        this.tipo = tipo;
+    }
+
+    public Empleado getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Empleado origen) {
+        this.origen = origen;
+    }
+
+    public Empleado getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Empleado destino) {
+        this.destino = destino;
+    }
+    
+    
     
 }
